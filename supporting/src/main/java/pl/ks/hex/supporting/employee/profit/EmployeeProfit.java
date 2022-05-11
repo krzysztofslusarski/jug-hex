fe67a4e364541647cd0e428abf2f8f7474b289c6
Krzysztof Ślusarski <pasqal@gmail.com>
2022-05-11 22:41:16 +0200

    Public

--- supporting/src/main/java/pl/ks/hex/supporting/employee/profit/EmployeeProfit.java
+++ supporting/src/main/java/pl/ks/hex/supporting/employee/profit/EmployeeProfit.java
@@ -1,25 +1,17 @@
 package pl.ks.hex.supporting.employee.profit;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import pl.ks.hex.common.model.Money;
 import pl.ks.hex.employee.EmployeeId;
+
+import javax.persistence.*;
 
 @Entity
 @Getter(AccessLevel.PUBLIC)
 @Setter(AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
-class EmployeeProfit {
+public class EmployeeProfit {
     @EmbeddedId
     @AttributeOverrides({
             @AttributeOverride(name = "value", column = @Column(name = "id"))
