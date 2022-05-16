435e2f3278f318d1d606eaa7207c779880f33e29
Krzysztof Ślusarski <pasqal@gmail.com>
2022-05-16 22:11:41 +0200

    Public

--- external-service-adapter/src/main/java/pl/ks/hex/police/PoliceConfiguration.java
+++ external-service-adapter/src/main/java/pl/ks/hex/police/PoliceConfiguration.java
@@ -3,7 +3,7 @@ package pl.ks.hex.police;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 class PoliceConfiguration {
     @Bean
     PoliceService policeService() {
